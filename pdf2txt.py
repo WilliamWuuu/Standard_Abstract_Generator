15b046385a9361c431e70a09eac4d7cd3dfcc6c1
Yangxin Wu <149234481+ILovAC@users.noreply.github.com>
2024-03-25 14:17:36 +0800

    Rewrite pdf2txt.py

--- pdf2txt.py
+++ pdf2txt.py
@@ -3,35 +3,37 @@ import pypdf
 
 def pdf_to_text(pdf_file):
     """将PDF文件转成txt文本"""
-    # 读取pdf文件
-    pdf_reader = pypdf.PdfReader(pdf_file)
-    # 获取pdf文件页数
-    page_num = len(pdf_reader.pages)
-    # 遍历每一页
     text = ''
-    for i in range(page_num):
-        # 获取每一页的文本内容
-        page_object = pdf_reader.pages[i]
-        text += page_object.extract_text()
+    with open(pdf_file, "rb") as pdf_file:
+        # 读取pdf文件
+        pdf_reader = pypdf.PdfReader(pdf_file)
+        # 获取pdf文件页数
+        page_num = len(pdf_reader.pages)
+        # 遍历每一页
+        for i in range(page_num):
+            # 获取每一页的文本内容
+            page_object = pdf_reader.pages[i]
+            text += page_object.extract_text()
     return text
 
-# 此处应为源PDF文件的父目录的绝对路径（可视情况该用相对路径）
-os.chdir("/Users/wyx/Desktop/files")   
-# 此处应为生成txt文件的父目录的绝对路径（可视情况改用相对路径）
-os.mkdir("text")   
+# 存放pdf文件的绝对路径
+pdf_path = '/Users/wyx/Desktop/Python Files/web/samples/GB'
+# 存放txt文件的绝对路径
+txt_path = '/Users/wyx/Desktop/Python Files/web/samples/results'
 
-# 遍历指定文件夹下的所有文件
-for filename in os.listdir("documents"):
-    print(f"\n{filename}", end="")
-    
-    text = pdf_to_text(f"documents/{filename}")
-    with open(f"text/{filename}.txt", "w") as file:
-        file.write(text)
-        count = text.count("G")
-    # 以"G"的个数来判定转换后的txt文本是否为无效文件   
-    if (count > 1000):
-        os.remove(f"documents/{filename}")
-        os.remove(f"text/{filename}.txt")
-        print("\t\tDeleted", end="")
+# 遍历文件夹中的文件
+for filename in os.listdir(pdf_path):
+    file_path = os.path.join(pdf_path, filename)
+    text = pdf_to_text(file_path)
 
-print("\n")
+    count = text.count("G")
+    if count < 1000:
+        # 定义新文件名
+        new_filename = f"{filename}.txt"
+        new_file_path = os.path.join(txt_path, new_filename)
+        
+        text = text.replace(' ', '')
+        # 写入操作后的结果到新文件中
+        with open(new_file_path, 'w') as new_file:
+            new_file.write(text)
+            print(f"已将转换后的结果写入文件：{new_file_path}")
