13dbdfdb6af765e8077ac356a3760d765f1cb10e
Yangxin Wu <149234481+WilliamWuuu@users.noreply.github.com>
2024-03-28 10:03:55 +0800

    handle unicode error

--- pdf2txt.py
+++ pdf2txt.py
@@ -15,16 +15,22 @@ def pdf_to_text(pdf_file):
             for i in range(page_num):
                 # 获取每一页的文本内容
                 page_object = pdf_reader.pages[i]
-                text += page_object.extract_text()
+                try:
+                    # 提取文本并追加到结果中
+                    text += page_object.extract_text()
+                except UnicodeEncodeError:
+                    # 在遇到编码错误时跳过当前文件
+                    print(f"UnicodeEncodeError: Skipping file {pdf_file.name}")
+                    break
         return text
     except PdfReadError as e:
         print(f"Error reading PDF: {e}")
         return None
 
 # 存放pdf文件的绝对路径
-pdf_path = '/Users/wyx/Desktop/Python Files/web/pdf_files'
+pdf_path = '/Users/wyx/Desktop/Python_Files/web/pdf_files'
 # 存放txt文件的绝对路径
-txt_path = '/Users/wyx/Desktop/Python Files/web/results'
+txt_path = '/Users/wyx/Desktop/Python_Files/web/text_files'
 
 # 遍历文件夹中的文件
 for filename in os.listdir(pdf_path):
@@ -40,7 +46,7 @@ for filename in os.listdir(pdf_path):
             
             text = text.replace(' ', '')
             # 写入操作后的结果到新文件中
-            with open(new_file_path, 'w') as new_file:
+            with open(new_file_path, 'w', encoding='utf-8', errors='replace') as new_file:
                 new_file.write(text)
                 print(f"已将转换后的结果写入文件：{new_file_path}")
                 
